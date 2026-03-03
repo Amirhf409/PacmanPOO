c40cda3a9932ca40667c2c9ee6f18e0bf723f3c7
Amir Hernández <amirhernandezfarah06@gmail.com>
2026-03-03 10:24:59 -0600

    Proyecto base Pacman POO

---
+++ src/main/java/java/pacmanpoo/PacmanPOO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package java.pacmanpoo;
+
+/**
+ *
+ * @author Amirh
+ */
+public class PacmanPOO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
